81e1e2bf24aebe63e351208efd4d041f6e7514eb
Code-For-Myself <jannissven@gmail.com>
2024-01-10 18:21:37 +0100

    init

--- Auswertung.py
+++ Auswertung.py
@@ -1,24 +1,27 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("Gold_Shutter483_dur90-001-alltracks.csv")
+data1 = pd.read_csv("Gold_Shutter483_dur90-001-alltracks.csv")
 
-X1 = np.array(data[" X1"])
-Y1 = np.array(data[" Y1"])
-X2 = np.array(data[" X2"])
-Y2 = np.array(data[" Y2"])
 
 particleID = 1
 
 
 indizes = []
-for i in range(0, len(data["Particle ID"])):
-    if (data["Particle ID"][i] == int(particleID)):
+for i in range(0, len(data1["Particle ID"])):
+    if (data1["Particle ID"][i] == int(particleID)):
         indizes.append(i)
-
+data = data1.loc[ indizes , [" X1"," Y1"," X2"," Y2"] ]
 def distSquared(x1,y1,x2,y2):
     ausgabe = (x2-x1)**2 + (y2-y1)**2
     return ausgabe
+
+
+
+X1 = np.array(data[" X1"])
+Y1 = np.array(data[" Y1"])
+X2 = np.array(data[" X2"])
+Y2 = np.array(data[" Y2"])
 
 summe = 0
 print(len(X1))
